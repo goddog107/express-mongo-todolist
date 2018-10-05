cce01fd12bce5b0c3abe0b850722583f8ab83d31
Sarah Jane <godwolf107@gmail.com>
2018-10-05 22:11:41 +0700

    create todo form

--- index.js
+++ index.js
@@ -1,32 +1,78 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
 
+// Database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todos');
+
+var todoSchema = mongoose.Schema({
+	title: String,
+	description: String,
+});
+var Todo = mongoose.model("Todo", todoSchema);
+
 app.get('/', function (req, res) {
-  res.send("Hello world!");
+	res.send("Hello world!");
 });
 
-app.get('/todos', function (req, res) {
-  res.send("Todo list");
-});
+app.post('/todo-create', function (req, res) {
+	var todoInfo = req.body;
+		
+	if(todoInfo == undefined) {
+		res.render('todo-create', {
+			message: "Please post right info",
+			type: "error"
+		});
+	}
+	else if (!todoInfo.title || !todoInfo.description) {
+		res.render('todo-create', {
+			message: "Sorry, you provided worng info",
+			type: "error"
+		});
+	} else {
+		var newTodo = new Todo({
+			title: todoInfo.title,
+			description: todoInfo.description,
+		});
 
-app.post('/todos/:id/mark_as_done', function (req, res) {
-  res.send("create todo, id: " + req.params.id);
+		newTodo.save(function (err, Todo) {
+			if (err)
+				res.render('todo-create', {
+					message: "Database error",
+					type: "error"
+				});
+			else
+				res.render('todo-create', {
+					message: "added",
+					type: "success",
+					todo: todoInfo
+				});
+		});
+	}
 });
 
 app.put('/todos/:id/update', function (req, res) {
-  res.send("update todo, id: " + req.params.id);
+	res.send("update todo, id: " + req.params.id);
 });
 
 app.delete('/todos/:id', function (req, res) {
-  res.send("delete todo, id: " + req.params.id);
+	res.send("delete todo, id: " + req.params.id);
 });
 
-app.get('/todo-list', function (req, res) {
-  res.render('todo-list');
+app.get('/todo-create', function (req, res) {
+	res.render('todo-create');
+});
+
+app.get('/todos', function (req, res) {
+	res.render('todo-list');
 });
 
 app.listen(3000);
